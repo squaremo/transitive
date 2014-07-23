bb67ed92f4fa9f3fd71262b4bba30d9aba2f5d27
Michael Bridgen <mikeb@squaremobius.net>
2014-07-23 08:02:22 +0100

    Echo server in netty
    Interesting weirdness: .addLast is overloaded in several ways, and it
    seems that you can't simply supply a ChannelHandler -- you have to
    supply a string and a handler, or an array of handlers. Possibly
    because the overload is addLast(ChannelHandler ...), and Clojure won't
    find this by reflection.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
-(defproject transitive "0.0.1"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [aleph "0.2.0"]])
+(defproject transitive "0.0.2"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.typed "0.2.53"]
+                 [org.clojure/tools.trace "0.7.8"]
+                 [co.paralleluniverse/pulsar "0.5.0"]
+                 [io.netty/netty-all "4.0.21.Final"]])
---
+++ src/transitive/netty.clj
@@ -0,0 +1,52 @@
+(ns transitive.netty
+  (import [io.netty.buffer ByteBuf]
+          [io.netty.bootstrap ServerBootstrap]
+          [io.netty.channel
+           Channel
+           ChannelHandlerContext
+           ChannelInboundHandlerAdapter
+           ChannelFuture
+           ChannelInitializer
+           ChannelOption
+           EventLoopGroup]
+          [io.netty.channel.nio NioEventLoopGroup]
+          [io.netty.channel.socket SocketChannel]
+          [io.netty.channel.socket.nio NioServerSocketChannel]))
+
+(defn make-channel-adapter []
+  (proxy [ChannelInboundHandlerAdapter] []
+    (channelRead
+      [^ChannelHandlerContext ctx buf]
+      (.write ctx buf))
+    (channelReadComplete [ctx]
+      (.flush ctx))
+    (exceptionCaught
+      [^ChannelHandlerContext ctx ^Throwable cause]
+      (.printStackTrace cause)
+      (.close ctx))))
+
+(defn make-channel-initializer []
+  (proxy [ChannelInitializer] []
+    (initChannel [^SocketChannel channel]
+      (doto (.pipeline channel)
+        (.addLast "echo" (make-channel-adapter))))))
+
+(defn print-new []
+  (println "new channel"))
+
+(defn make-channel-reporter []
+  (proxy [ChannelInitializer] []
+    (initChannel [^Channel ch] (print-new))))
+
+(defn make-listener []
+  (let [bosses  (NioEventLoopGroup.)
+        workers (NioEventLoopGroup.)
+        shutdown #(do (.shutdownGracefully bosses)
+                      (.shutdownGracefully workers))]
+    (let [boot (-> (ServerBootstrap.)
+                   (.group bosses workers)
+                   (.channel NioServerSocketChannel)
+                   (.childHandler (make-channel-adapter))
+                   (.option ChannelOption/SO_BACKLOG (int 128))
+                   #_(.childOption ChannelOption/SO_KEEPALIVE true))]
+      [boot, shutdown])))
