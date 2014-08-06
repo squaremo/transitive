ff70ac942146c31396b2df13c2e4c75cb41dc8e2
Michael Bridgen <mikeb@squaremobius.net>
2014-08-07 00:13:22 +0100

    Project file no-ops
    Add a plugin for cider support and a non-dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
                  [org.clojure/tools.trace "0.7.8"]
                  #_[co.paralleluniverse/pulsar "0.6.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 #_[org.van-clj/zetta-parser "0.0.4"]
                  [io.netty/netty-all "4.0.21.Final"]]
+
+  :plugins [[cider/cider-nrepl "0.7.0-SNAPSHOT"]]
+
   :main transitive.main
 
 ;;  :java-agents [[co.paralleluniverse/quasar-core "0.6.0"]]
